d0651df9f3a43abeeead18a5b10f8dd3dd5d061c
Elias Menezes <167769118+EliasMenezes@users.noreply.github.com>
2024-05-14 21:39:11 -0300

    Add files via upload

---
+++ Culinaria_Facil-main/banco_de_dados/banco_de_dados_pedrolucas.sql
@@ -0,0 +1,60 @@
+create database sitereceita;
+use sitereceita;
+
+create table usuario(
+id int auto_increment primary key,
+nome varchar(100) not null,
+email varchar(100) not null unique,
+senha varchar(255) not null,
+cpf varchar(11) unique,
+imagem_perfil varchar(255)
+);
+
+
+
+CREATE TABLE categorias (
+categoriaID INT AUTO_INCREMENT PRIMARY KEY,
+categoriaNome VARCHAR(200),
+Descricao TEXT
+);
+
+CREATE TABLE receitas (
+receitaID INT AUTO_INCREMENT PRIMARY KEY,
+Titulo VARCHAR(55),
+Descricao TEXT,
+Instrucoes TEXT,
+ingredientes TEXT,
+TempoPreparo INT,
+Dificuldade VARCHAR(50),
+CategoriaID INT,
+AutorID INT,
+data_hora datetime,
+imagem_receita varchar(255),
+video_receita varchar(255),
+FOREIGN KEY (CategoriaID) REFERENCES categorias(categoriaID),
+foreign key (autorID) references usuario(id)
+);
+
+CREATE TABLE comentarios (
+id int auto_increment primary key, 
+receitaID int,
+usuarioID int,
+comentario text,
+data_hora datetime,
+foreign key (receitaID) references receitas (receitaID),
+foreign key (usuarioID) references usuario(id)
+);
+
+select * from receitas;
+
+select * from usuario;
+
+select * from comentarios;
+
+insert into categorias(categorianome, descricao) values
+('Vegano', 'Descrição da categoria'),
+('Sem Glútem', 'Descrição da categoria'),
+('Massas', 'Descrição da categoria'),
+('Doces e Sobremesas', 'Descrição da categoria'),
+('Bebidas e Drinks', 'Descrição da categoria'),
+('Carnes', 'Descrição da categoria');
